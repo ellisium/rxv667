0f0ed6cb42a2c6a2f81993fb0562e7c12b854817
Eric Le Lay <eric.lelay@gmail.com>
2015-09-01 18:43:51 +0200

    progress comit

--- rxv667.js
+++ rxv667.js
@@ -3,12 +3,35 @@ SerialPort = serialPort.SerialPort,
 settings=require(__dirname+"\\settings.json"),
 ir=require(__dirname+"\\cmdIR.js");
 
+var readyYamaha=false;
+
 const yamaha = new SerialPort(settings.port, {baudrate: settings.baudrate}, false);
+yamaha.open(function(err) {
+	if(err) {console.log(err)}
+	yamaha.on('data', function(data){
+		var str=data.toString();
+		if(str.match('ready')){
+			console.log('yamaha connected')
+			readyYamaha=true;
+		} else{
+			console.log(str)
+		}
+	 });
+});
 
 exports.action = function(params, next){
-
-  console.log(params)
-  // Callback with TTS
-  next({'tts': 'ok'});
-
+	if(readyYamaha){
+	  if(params.hasOwnProperty('cmd')){
+	  	var item=ir[params.cmd],
+	  	code=item.code,
+	  	res=item.vocals;
+	  	yamaha.write(new Buffer(code+'\n'));
+	  	var choix = Math.floor(Math.random() * res.length); 
+		next({tts:res[choix]});
+	  }else{
+	  	next({tts:"Je n'ai pas compri pouvez vous répété"});
+	  }
+	}else{
+		next({tts:"La connection infrarouge n'est pas encore initialisé, essayé dans quelques secondes"});
+	}
 }
